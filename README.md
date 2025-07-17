a980975e90ab86acca69261195a602e18ca9e2f5
kortingjulid <kortingjulid@gmail.com>
2025-07-17 20:13:17 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# abgngewe
-abgngewe
+# [abgngewe](https://abgngewe.pages.dev)
---
+++ src/utils/site.js
@@ -0,0 +1,9 @@
+export const nama = 'Abg Ngewe';
+export const desk = 'Video Bokep Indo Bocil Ngentot Jilbab Smp Mama Viral Sma Cindo Tante China';
+export const url = 'https://abgngewe.pages.dev';
+export const terbit = '2024-12-17T00:01:03Z';
+
+export const google = 'dzNqvBNufkxRDPtXsRmv3sEKR8ICiEe0cJAZFTxYDmE';
+export const yandex = '89fe2c43be68153d'
+
+export const index = 'adce6f08-dcf4-4424-8c2f-f14cc68bc9db';
